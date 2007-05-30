5b38d1558aa83cd40cbd20d6c3680e24b1f1c445
Christian Zagrodnick <cz@flyingcircus.io>
2007-05-30 16:20:20 +0000

    importing not quite finished mochikit integration into zope3

---
+++ setup.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'gocept.fckeditor',
+    version = "2.4",
+    author = "Christian Zagrodnick",
+    author_email = "cz@gocept.com",
+    description = "FCKEditor integration into Zope 3",
+    long_description = "XXX",
+    license = "ZPL 2.1",
+    url='https://svn.gocept.com/repos/gocept/gocept.mochikit',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    include_package_data = True,
+    zip_safe = False,
+
+    namespace_packages = ['gocept'],
+    install_requires = [
+        'setuptools',
+        'zc.resourcelibrary',
+    ],
+    extras_require = {
+        'test': ['zope.testing',
+                 'zope.testbrowser',
+                 'zope.app.testing',
+                 'zope.app.zcmlfiles',
+                ],
+    },
+    )
