51c0ed7a073a1d49b4cccaa704fdd5dffaaddc4b
Christian Zagrodnick <cz@flyingcircus.io>
2009-04-20 11:37:57 +0000

    * Made unregistering of event handlers more reliable in lightbox.
    * Allow to not use ids for the lightbox.
    
    (we really should factor that all out)

--- src/gocept/mochikit/resources/lightbox.js
+++ src/gocept/mochikit/resources/lightbox.js
@@ -2,28 +2,56 @@
 
 gocept.Lightbox = gocept.Class.extend({
 
-    construct: function(parent_element) {
+    default_options: {
+        use_ids: true,
+    },
+
+    construct: function(parent_element, option_args) {
+        var options = MochiKit.Base.clone(this.default_options);
+        if (!isUndefinedOrNull(option_args)) {
+            MochiKit.Base.update(options, option_args);
+        }
+
         this.parent_element = parent_element;
-        this.shade = DIV({'id': 'lightbox-shade'})
-        this.header = DIV({'id': 'lightbox-header'});
+        this.shade = DIV({'class': 'lightbox-shade'})
+        this.header = DIV({'class': 'lightbox-header'});
         this.closebutton = A({'href': '#', 
                               'title': 'Close',
                               'class': 'CloseButton'});
-        this.content_box = DIV({'id': 'lightbox'});
-        appendChildNodes(this.header, this.closebutton);
-        appendChildNodes(this.parent_element, this.shade, this.header, this.content_box);
-        connect(this.closebutton, 'onclick', this, 'close');
-        connect(this.shade, 'onclick', this, 'close');
+        this.content_box = DIV({'class': 'lightbox'});
 
+        if (options.use_ids) {
+            forEach([this.shade, this.header, this.content_box],
+                    function(element) {
+                        element.id = element.getAttribute('class');
+            });
+        }
+
+        MochiKit.DOM.appendChildNodes(this.header, this.closebutton);
+        MochiKit.DOM.appendChildNodes(
+            this.parent_element, this.shade, this.header, this.content_box);
+        this.events = []
+        this.events.push(
+            MochiKit.Signal.connect(
+                this.closebutton, 'onclick', this, this.close));
+        this.events.push(
+            MochiKit.Signal.connect(
+                this.shade, 'onclick', this, this.close));
     },
 
     close: function() {
         MochiKit.Signal.signal(this, 'before-close');
-        disconnectAll('lightbox-shade');
-        disconnectAll('lightbox');
-        removeElement('lightbox-shade');
-        removeElement('lightbox');
-        removeElement('lightbox-header');
+        while (this.events.length) {
+            MochiKit.Signal.disconnect(this.events.pop());
+        }
+
+        forEach([this.shade, this.header, this.content_box],
+                function(element) {
+                    MochiKit.Signal.disconnectAll(element);
+                    if (!isNull(element.parentNode)) {
+                        MochiKit.DOM.removeElement(element);
+                    }
+        });
     },
     
     load_url: function(url, query) {
@@ -49,7 +77,7 @@ gocept.Lightbox = gocept.Class.extend({
     },
 
     replace_content: function(new_html) {
-        $('lightbox').innerHTML = new_html;
+        this.content_box.innerHTML = new_html;
     },
 
 });
